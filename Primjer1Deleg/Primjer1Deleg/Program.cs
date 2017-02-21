6c05347e91be69d35b8e943c221ec760f879ef41
sandro sviben <sandro.sviben@hotmail.com>
2017-02-21 13:56:58 +0100

    primjer1Delegati

---
+++ Primjer1Deleg/Primjer1Deleg/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public delegate void HelloFuntionDelegate(string Message);
+
+namespace Primjer1Deleg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloFuntionDelegate del = new HelloFuntionDelegate(Hello);//proslijediti ime metode koja mora biti void
+            del("Hello from Delegate");
+        }
+
+        public static void Hello(string strMessage)
+        {
+            Console.WriteLine(strMessage);
+        }
+    }
+
+}
