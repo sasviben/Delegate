c60545b29ab19971d39f2c2a31cc0caded5e0da1
sandro sviben <sandro.sviben@hotmail.com>
2017-02-21 15:24:25 +0100

    Multicas delegat - pokazuje na više metoda

---
+++ Primjer1Deleg/MulticastDelegate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public delegate void SampleDelegete();
+
+namespace MulticastDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleDelegete del1, del2, del3, del4;
+            del1 = new SampleDelegete(SampleMethodOne);
+            del2 = new SampleDelegete(SampleMethodTwo);
+            del3 = new SampleDelegete(SampleMethodThree);
+
+            del4 = del1 + del2 + del3;
+            del4();
+        }
+        public static void SampleMethodOne()
+        {
+            Console.WriteLine("SampleMethodOne Invoked");
+        }
+        public static void SampleMethodTwo()
+        {
+            Console.WriteLine("SampleMethodTwo Invoked");
+        }
+        public static void SampleMethodThree()
+        {
+            Console.WriteLine("SampleMethodThree Invoked");
+        }
+    }
+}
