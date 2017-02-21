5bdbdce62b7a53eace091bd2397faa5771d5f4ab
sandro sviben <sandro.sviben@hotmail.com>
2017-02-21 14:23:02 +0100

    Primjer2

---
+++ Primjer1Deleg/Primjer2Deleg/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primjer2Deleg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> empList = new List<Employee>();
+            empList.Add(new Employee() { ID = 101, Name = "Mark", Salary = 3000, Experinece = 3 });
+            empList.Add(new Employee() { ID = 101, Name = "Sandro", Salary = 5000, Experinece = 5 });
+            empList.Add(new Employee() { ID = 101, Name = "Filip", Salary = 4000, Experinece = 4 });
+            empList.Add(new Employee() { ID = 101, Name = "Hrvoje", Salary = 7000, Experinece = 7 });
+
+            Employee.PromotedEmployee(empList);
+        }
+    }
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experinece { get; set; }
+
+        public static void PromotedEmployee(List<Employee> employeeList)
+        {
+            foreach(Employee emp in employeeList)
+            {
+                if(emp.Experinece >= 5)
+                {
+                    Console.WriteLine(emp.Name + " promoted");
+                }
+            }
+        }
+    }
+}
